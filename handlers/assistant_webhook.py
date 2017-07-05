7d9365837dd2a01157fae546c854a47ae96eb567
Zack-- <zdibe@google.com>
2017-07-05 17:42:48 +0100

    remove unnecessary code

--- handlers/assistant_webhook.py
+++ handlers/assistant_webhook.py
@@ -1,24 +1,15 @@
 import json
 
-import apiaiassistant
-
 from handlers import base
 from agent import assistant
 
 
 class AssistantWebhook(base.BaseAjaxHandler):
 
-    def apiai_error(self):
-        self.render_json({'error': '400'})
-        return
-
     def post(self):
         agent = assistant.process(
             json.loads(self.request.body),
             headers=self.request.headers
         )
-
-        if agent.code != apiaiassistant.agent.Status.OK:
-            return self.apiai_error()
 
         self.render_json(agent.response.to_dict())
